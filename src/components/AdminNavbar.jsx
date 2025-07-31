457654b29b0d82c8bf996bbbe2a7fc2f1cea9919
Nirbhik Shrestha <nirbhik2060@gmail.com>
2025-07-31 18:45:16 +0545

    Added minor edits

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -6,7 +6,6 @@ export default function AdminNavbar({ onLogout }) {
     <nav className="navbar">
       <div className="nav-links">
         <Link to="/admin/dashboard">Dashboard</Link>
-        {/* Add more admin links here when you build them */}
       </div>
       <div className="navbar-right">
         <button className="logout-button" onClick={onLogout}>
